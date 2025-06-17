05675360f720ac7bfe034875e0a6b16fbd628c7a
Davi Kazuhiro <daviknatume@gmail.com>
2025-06-17 16:54:32 -0300

    pasta templates desnecessaria

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from utils.create_db import create_db  # Optional, only if you're using this
 if __name__ == "__main__":
     app = create_app()
 
-    # Uncomment this line if you want to initialize the DB schema
     # create_db(app)
 
-    # Start the Flask server with logs and auto-reload
     app.run(host='127.0.0.1', port=8080, debug=True)
